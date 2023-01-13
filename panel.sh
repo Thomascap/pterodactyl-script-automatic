d1aa52dff5af80128b09cf57575f4c4d6ebaa5f1
Thomascaptein <42696313+Thomascap@users.noreply.github.com>
2023-01-13 21:56:36 +0100

    update

--- panel.sh
+++ panel.sh
@@ -9,8 +9,14 @@
 USE_DOMAIN=false
 USE_SSL=false
 MYSQL_PASSWORD=`head -c 8 /dev/random | base64`
+EMAIL="<email>"
+USERNAME="<username>"
+FIRSTNAME="<firstname>"
+LASTNAME="<lastname>"
+PASSWORD=`head -c 8 /dev/random | base64`
 
 dependency_install() {
+    echo 'MESSAGE1' > data.txt && sleep 5
     apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
     LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
     curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
@@ -22,10 +28,12 @@ dependency_install() {
 }
 
 installing_composer() {
+    rm data.txt && echo 'MESSAGE2' > data.txt && sleep 5
     curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 }
 
 download_files() {
+    rm data.txt && echo 'MESSAGE3' > data.txt && sleep 5
     mkdir -p /var/www/pterodactyl
     cd /var/www/pterodactyl
     curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
@@ -34,6 +42,7 @@ download_files() {
 }
 
 database_configuration() {
+    rm data.txt && echo 'MESSAGE4' > data.txt && sleep 5
     mysql -u root -e "CREATE USER 'pterodactyl'@'127.0.0.1' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -u root -e "CREATE DATABASE panel;"
     mysql -u root -e "GRANT ALL PRIVILEGES ON panel.* TO 'pterodactyl'@'127.0.0.1' WITH GRANT OPTION;"
@@ -41,12 +50,14 @@ database_configuration() {
 }
 
 installation() {
+    rm data.txt && echo 'MESSAGE5' > data.txt && sleep 5
     cp .env.example .env
     composer install --no-dev --optimize-autoloader
     php artisan key:generate --force
 }
 
 environment_configuration() {
+    rm data.txt && echo 'MESSAGE6' > data.txt && sleep 5
     if [ "$USE_SSL" == true ]; then
         php artisan p:environment:setup --author=$EMAIL --url=https://$FQDN --timezone=Europe/Amsterdam --cache=redis --session=redis --queue=redis --redis-host=127.0.0.1 --redis-pass=null --redis-port=6379 --settings-ui=true
     elif [ "$USE_SSL" == false ]; then
@@ -56,29 +67,35 @@ environment_configuration() {
 }
 
 database_setup() {
+    rm data.txt && echo 'MESSAGE7' > data.txt && sleep 5
     php artisan migrate --seed --force
 }
 
 add_the_first_user() {
-    php artisan p:user:make
+    rm data.txt && echo 'MESSAGE8' > data.txt && sleep 5
+    php artisan p:user:make --email=$EMAIL --username=$USERNAME --name-first=$FIRSTNAME --name-last=$LASTNAME --password=$PASSWORD --admin=1
 }
 
 set_permissions() {
+    rm data.txt && echo 'MESSAGE8' > data.txt && sleep 5
     chown -R www-data:www-data /var/www/pterodactyl/*
 }
 
 crontab_configuration() {
+    rm data.txt && echo 'MESSAGE9' > data.txt && sleep 5
     cronjob="* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1"
     (crontab -u root -l; echo "$cronjob" ) | crontab -u root -
 }
 
 create_queue_worker() {
+    rm data.txt && echo 'MESSAGE10' > data.txt && sleep 5
     curl -o /etc/systemd/system/pteroq.service https://raw.githubusercontent.com/Thomascap/pterodactyl-script-automatic/pteroq.service
     sudo systemctl enable --now redis-server
     sudo systemctl enable --now pteroq.service
 }
 
 certbot() {
+    rm data.txt && echo 'MESSAGE11' > data.txt && sleep 5
     sudo apt update
     sudo apt install -y certbot
     sudo apt install -y python3-certbot-nginx
@@ -88,6 +105,7 @@ certbot() {
 }
 
 webserver_configuration() {
+    rm data.txt && echo 'MESSAGE12' > data.txt && sleep 5
     rm /etc/nginx/sites-enabled/default
     if [ "$USE_SSL" == true ]; then
         certbot_usage
